819fe296fe312bfefd0cad7b01c72fabf2dc7e57
chris <phfae23@student.sdu.dk>
2024-08-08 23:30:18 +0200

    Der er pågyndt struktur, lavet start matrix klase

---
+++ libs/vector3d.hpp
@@ -0,0 +1,16 @@
+#ifndef VECTOR3D_HPP
+#define VECTOR3D_HPP
+
+#include <vector>
+#include <iostream>
+
+template <typename T>
+class Vec3d {};
+
+template <typename T>
+class Vec3d {};
+
+template <typename T>
+class Vec3d {};
+
+#endif // VECTOR3D_HPP
