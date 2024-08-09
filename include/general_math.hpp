2fedeb4936c09483392df7f193fd1e7f30511a24
Christopher <cf@4x-robots.com>
2024-08-09 15:02:56 +0200

    custom int is made

--- include/general_math.hpp
+++ include/general_math.hpp
@@ -5,7 +5,52 @@
 
 
 namespace math{
+class c_int{
+    
+    private: 
+        int16_t _int;
+    
+    public: 
+        c_int(){
+            _int = 0;
+        }
+        c_int(int16_t i){
+            _int = i;
+        }
+        void set_val(int16_t i){
+            _int = i;
+        }
+        int16_t get_val(){
+            return _int;
+        }
+        c_int operator+(c_int i){
+            return c_int(_int + i.get_val());
+        }
+        c_int operator-(c_int i){
+            return c_int(_int - i.get_val());
+        }
+        c_int operator*(c_int i){
+            return c_int(_int * i.get_val());
+        }
+        c_int operator/(c_int i){
+            return c_int(_int / i.get_val());
+        }
+        c_int operator%(c_int i){
+            return c_int(_int % i.get_val());
+        }
+        double operator^(c_int i){
+            double temp; 
+            for (int j = 0; j < i.get_val(); j++){
+                temp *= _int;
+            }
+            return temp;
+        }
+        void print(){
+            std::cout << _int << std::endl;
+        }
+        
 
+};
 
 
 
